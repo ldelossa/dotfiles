1aadee2d89bceb20bd8b3c8d0d0a9aa815434700
ldelossa <louis.delos@gmail.com>
2021-10-04 17:42:18 -0400

    fix border nudging windows on focus

--- nvim/nvim-lsp.lua
+++ nvim/nvim-lsp.lua
@@ -8,6 +8,7 @@ local nvim_lsp = require('lspconfig')
 -- disable virtual text for all diagnostic handlers.
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
+        underline    = false,
         virtual_text = false,
     }
 )
