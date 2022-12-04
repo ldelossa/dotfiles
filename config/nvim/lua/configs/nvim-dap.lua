70f483bb21a3549f1ad808813f5f3a363b66fd68
Louis DeLosSantos <louis.delos@gmail.com>
2022-12-04 12:45:18 -0500

    nvim-dap hoops

--- config/nvim/lua/configs/nvim-dap.lua
+++ config/nvim/lua/configs/nvim-dap.lua
@@ -1,1 +1,24 @@
 require('dapui').setup()
+
+local function close_nvim_ide_panels() 
+    if pcall(require, 'ide') then
+        local ws = require('ide.workspaces.workspace_registry').get_workspace(vim.api.nvim_get_current_tabpage())
+        if ws ~= nil then
+            ws.close_panel(require('ide.panels.panel').PANEL_POS_BOTTOM)
+            ws.close_panel(require('ide.panels.panel').PANEL_POS_LEFT)
+            ws.close_panel(require('ide.panels.panel').PANEL_POS_RIGHT)
+        end
+    end
+end
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+  close_nvim_ide_panels()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
