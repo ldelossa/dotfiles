c262bc9a4538f87e71c672ce76efd4bf98174ea4
Louis DeLosSantos <louis.delos@gmail.com>
2023-01-04 21:26:15 -0500

    devspace prompt

--- zsh.d/zsh.sh
+++ zsh.d/zsh.sh
@@ -12,6 +12,12 @@ zstyle :prompt:pure:git:stash color 031
 PURE_PROMPT_SYMBOL="🖳 "
 PURE_PROMPT_VICMD_SYMBOL=" "
 
+# if we are running in a DEVSPACE container, make our prompt indicate this.
+if [ -n "$DEVSPACE" ]; then
+    PURE_PROMPT_SYMBOL="($DEVSPACE)🖳 "
+    PURE_PROMPT_VICMD_SYMBOL="($DEVSPACE) "
+fi
+
 # read from compinit
 autoload -Uz compinit
 
