cf632058d3c216851d5c5947f741e851c419d594
Louis DeLosSantos <louis.delos@gmail.com>
2023-02-12 09:10:35 -0500

    expand container monitor to podman

--- config/waybar/custom/containers-monitor.sh
+++ config/waybar/custom/containers-monitor.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 IFS=$'\n'
 
-containers=($(docker ps --format "{{ .Names }}\t{{ .Status }}" | column -t))
-n="${#containers[@]}"
+docker_containers=($(docker ps --format "{{ .Names }}\t{{ .Status }}" | column -t))
+n="${#docker_containers[@]}"
+
+podman_containers=($(podman ps --format "{{ .Names }}\t{{ .Status }}" | column -t))
+n=$((n + "${#docker_containers[@]}"))
 
 tooltip="Containers:"
-for con in "${containers[@]}";
+for con in "${docker_containers[@]}";
+do
+    tooltip="$tooltip\n$con"
+done
+
+for con in "${podman_containers[@]}";
 do
     tooltip="$tooltip\n$con"
 done
