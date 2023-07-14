6fb33551e46a5a8939b1130934a50485ff53d16b
Louis DeLosSantos <louis.delos@gmail.com>
2023-07-14 06:58:19 -0400

    fix broken battery icons, remove deprecated gitsigns key

--- config/nvim/lua/configs/gitsigns.lua
+++ config/nvim/lua/configs/gitsigns.lua
@@ -10,44 +10,6 @@ require('gitsigns').setup {
   numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-  keymaps = {
-    -- Default keymap options
-    noremap = true,
-
-    ['n <leader>h'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
-    ['n <leader>hh'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
-
-    ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-    ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-    ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-    ['n <leader>hU'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
-    ['n <leader>hd'] = '<cmd>lua require"gitsigns".diffthis()<CR>',
-
-    -- c-g namespace
-    ['n <C-g>n'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
-    ['n <C-g>p'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
-    ['n <C-g>s'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['v <C-g>s'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <C-g>u'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <C-g>U'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
-    ['n <C-g>r'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['v <C-g>r'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <C-g>R'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-    ['n <C-g>pp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ['n <C-g>bb'] = '<cmd>lua require"gitsigns".blame_line({full=true})<CR>',
-    ['n <C-g>S'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-    ['n <C-g>d'] = '<cmd>lua require"gitsigns".diffthis()<CR>',
-
-    -- Text objects
-    ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-    ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
-  },
   watch_gitdir = {
     interval = 1000,
     follow_files = true
