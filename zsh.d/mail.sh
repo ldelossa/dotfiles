3b727d0b45b47d4f0fd2f1a9f33cdfdc8fb9cf21
ldelossa <louis.delos@gmail.com>
2023-07-21 07:16:23 -0400

    fix mail aliases

--- zsh.d/mail.sh
+++ zsh.d/mail.sh
@@ -1,5 +1,5 @@
 alias mutt=neomutt
 alias mail=mutt -f
-alias mail-bpf=mutt -f ~/Mail/linux-bpf-next
-alias mail-bpf-me=mutt -f ~/Mail/linux-bpf-me
-alias mail-up=lei up ~/Mail/linux-bpf-next ~/Mail/linux-bpf-me
+alias mail-bpf="mutt -f ~/Mail/linux-bpf-next"
+alias mail-bpf-me="mutt -f ~/Mail/linux-bpf-me"
+alias mail-up="lei up ~/Mail/linux-bpf-next ~/Mail/linux-bpf-me"
