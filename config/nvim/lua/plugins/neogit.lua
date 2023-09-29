f5116ce2b8053f707f7e0f15223f03687c5d16d7
Louis DeLosSantos <louis.delos@gmail.com>
2023-09-29 16:26:09 -0400

    lazynvim it is

--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -1,4 +1,15 @@
+vim.g.lazygit_use_custom_config_file_path = 1 -- config file path is evaluated if this value is 1
+vim.g.lazygit_config_file_path = '~/.config/lazygit/config.yml' -- custom config file path
+
 return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>gn"] = { name = "+Neogit" },
+      },
+    },
+  },
   {
     "NeogitOrg/neogit",
     dependencies = {
@@ -8,5 +19,11 @@ return {
       "ibhagwan/fzf-lua",              -- optional
     },
     config = true,
+    keys = {
+      { "<leader>gnn", "<cmd>Neogit kind=auto<cr>", desc = "Open Neogit" },
+      { "<leader>gnt", "<cmd>Neogit<cr>",               desc = "Open Neogit (Tab)" },
+      { "<leader>gnv", "<cmd>Neogit kind=vsplit<cr>",   desc = "Open Neogit (VSplit)" },
+      { "<leader>gns", "<cmd>Neogit kind=split<cr>",    desc = "Open Neogit (Split)" },
+    },
   },
 }
