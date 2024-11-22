4824a5243569531af49e18f17029c0c4550114b8
ldelossa <louis.delos@gmail.com>
2024-11-21 19:32:53 -0500

    add krun-node

--- zsh.d/kubectl.sh
+++ zsh.d/kubectl.sh
@@ -1,5 +1,5 @@
 # kubectl aliases
-kns="kube-system"
+export kns="kube-system"
 kconfig="$HOME/.kube/config"
 function kns() {
     if [[ -z  $1 ]]; then
@@ -81,4 +81,16 @@ function kp-netns() {
 	for x in $(k get pods -o custom-columns=NAME:.metadata.name --no-headers); do
 		echo -e "$x\t$(kexec "$x" -- readlink /proc/1/ns/net 2>/dev/null)"
  	done | column -t -s $'\t'
+}
+
+# [NAME] [IMAGE] [NODE] [CMD (optional)]
+function krun-node() {
+	cmd=$(cat << EOF
+$preamble run $1 -it --image $2 --overrides="{\"apiVersion\": \"v1\", \"spec\": { \"nodeName\": \"$3\" }}"
+EOF
+)
+	if [[ -n ${4} ]]; then
+		cmd="$cmd -- $4"
+	fi
+	eval $cmd
 }
