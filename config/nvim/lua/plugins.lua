cb17db03be04cfcd23debdc83a23e1fa87b9f63a
ldelossa <louis.delos@gmail.com>
2025-03-05 15:57:27 -0500

    use vertical layout for vimdiff

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -676,5 +676,36 @@ now(function()
 	add({
 		source = "sindrets/diffview.nvim",
 	})
-	require("diffview").setup()
+	require("diffview").setup({
+    view = {
+      -- Configure the layout and behavior of different types of views.
+      -- Available layouts:
+      --  'diff1_plain'
+      --    |'diff2_horizontal'
+      --    |'diff2_vertical'
+      --    |'diff3_horizontal'
+      --    |'diff3_vertical'
+      --    |'diff3_mixed'
+      --    |'diff4_mixed'
+      -- For more info, see |diffview-config-view.x.layout|.
+      default = {
+        -- Config for changed files, and staged files in diff views.
+        layout = "diff2_vertical",
+        disable_diagnostics = false,  -- Temporarily disable diagnostics for diff buffers while in the view.
+        winbar_info = false,          -- See |diffview-config-view.x.winbar_info|
+      },
+      merge_tool = {
+        -- Config for conflicted files in diff views during a merge or rebase.
+        layout = "diff3_vertical",
+        disable_diagnostics = true,   -- Temporarily disable diagnostics for diff buffers while in the view.
+        winbar_info = true,           -- See |diffview-config-view.x.winbar_info|
+      },
+      file_history = {
+        -- Config for changed files in file history views.
+        layout = "diff2_vertical",
+        disable_diagnostics = false,  -- Temporarily disable diagnostics for diff buffers while in the view.
+        winbar_info = false,          -- See |diffview-config-view.x.winbar_info|
+      }
+		}
+	})
 end)
