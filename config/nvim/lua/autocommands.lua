09ec4f487084cde446fa1a5c66a9ddc3e7a8c3e3
ldelossa <louis.delos@gmail.com>
2025-11-14 08:57:43 -0500

    spell check automatic in git commits

--- config/nvim/lua/autocommands.lua
+++ config/nvim/lua/autocommands.lua
@@ -63,3 +63,12 @@ vim.api.nvim_create_autocmd("VimLeavePre", {
 		})
 	end
 })
+
+-- enable spell check for git commit messages
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "gitcommit" },
+	callback = function()
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = "en_us"
+	end,
+})
