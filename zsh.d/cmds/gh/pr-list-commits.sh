0021e8776ef34b228b4beb50b30db0c34bdf8a2d
ldelossa <louis.delos@gmail.com>
2025-11-17 10:31:08 -0500

    fix commit lookup in pr-list-commits.sh

--- zsh.d/cmds/gh/pr-list-commits.sh
+++ zsh.d/cmds/gh/pr-list-commits.sh
@@ -4,15 +4,22 @@ args=("--repo:The repository the PR was opened againt" \
 help=("pr-list-commits", "List commits in a Github pull request.
 
  Given a repository and a pull request number provide a list of commits which
- make up the pull request's contents.
+ make up the pull request's contents in the local repository
 
  The pull argument omits the leading '#' of the pull request number.
 
- Commits are displayed in chronological order (oldest -> newest).
+ Commits are displayed in chronological order (oldest -> newest) in merge order.
+ The merge order may be different then the commit timestamps.
+
+ This search is not perfect since the commit IDs in the pull request may be
+ different from the commit IDs in the local repository. This command uses
+ 'git log --grep' to search for an exact match of the commit's subject string.
  ")
 
 execute() {
-	gh pr view "${pull}" --repo ${repo} --json commits --jq '.commits[]| "\(.oid[0:7]) \(.messageHeadline) \(.authoredDate)"'
+	IFS=$'\n'
+	messages=($(gh pr view "${pull}" --repo ${repo} --json commits --jq '.commits[]| "\(.messageHeadline)"'))
+	for x in "${messages[@]}"; do git log --format="%H | %s | %ai" --grep "$x"; done
 }
 
 
